a742c160f0d286cf604e040fcbeb88f792aaf582
Jorge L. Dorta <jldorta@gmail.com>
2023-04-15 00:24:32 -0400

    FIX: CI 4.2 Compatibility

--- src/Autoload.php
+++ src/Autoload.php
@@ -1,9 +1,1 @@
 <?php
-$routes = service('routes');
-
-//$routes->add('/api/(:any)', 'SimpleRESTfullApi\Api::$1');
-
-$routes->get('/api(:any)', 'SimpleRESTfullApi\Api::get$1');
-$routes->post('/api(:any)', 'SimpleRESTfullApi\Api::post$1');
-$routes->put('/api(:any)', 'SimpleRESTfullApi\Api::put$1');
-$routes->delete('/api(:any)', 'SimpleRESTfullApi\Api::delete$1');
---
+++ src/Config/Routes.php
@@ -0,0 +1,8 @@
+<?php 
+
+//$routes->add('/api/(:any)', 'SimpleRESTfullApi\Api::$1');
+
+$routes->get('/api(:any)', 'SimpleRESTfullApi\Api::get$1');
+$routes->post('/api(:any)', 'SimpleRESTfullApi\Api::post$1');
+$routes->put('/api(:any)', 'SimpleRESTfullApi\Api::put$1');
+$routes->delete('/api(:any)', 'SimpleRESTfullApi\Api::delete$1');
